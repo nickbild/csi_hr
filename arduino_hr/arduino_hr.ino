d1dc1b1fb80d16e67e28f1b36f3ca896a9c759a4
Nick Bild <nick.bild@gmail.com>
2025-09-18 17:11:31 -0400

    Update documentation.

---
+++ arduino_hr/arduino_hr.ino
@@ -0,0 +1,65 @@
+#include <Wire.h>
+#include "MAX30105.h"
+#include "heartRate.h"
+
+MAX30105 particleSensor;
+
+const byte RATE_SIZE = 4;  // Number of measurements to average.
+byte rates[RATE_SIZE];     // Array of heart rates.
+byte rateSpot = 0;
+long lastBeat = 0;  // Time at which the last beat occurred.
+
+float beatsPerMinute;
+int beatAvg;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Initializing...");
+
+  // Initialize sensor
+  if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) {
+    Serial.println("MAX30102 was not found. Please check wiring/power. ");
+    while (1)
+      ;
+  }
+  Serial.println("Place your index finger on the sensor with steady pressure.");
+
+  particleSensor.setup();                     // Configure sensor with default settings
+  particleSensor.setPulseAmplitudeRed(0x0A);  // Turn Red LED to low to indicate sensor is running
+  particleSensor.setPulseAmplitudeGreen(0);   // Turn off Green LED
+}
+
+void loop() {
+  long irValue = particleSensor.getIR();
+
+  if (checkForBeat(irValue) == true) {
+    // Sensed a beat.
+    long delta = millis() - lastBeat;
+    lastBeat = millis();
+
+    beatsPerMinute = 60 / (delta / 1000.0);
+
+    if (beatsPerMinute < 255 && beatsPerMinute > 20) {
+      rates[rateSpot++] = (byte)beatsPerMinute;
+      rateSpot %= RATE_SIZE;                     
+
+      // Calculate average.
+      beatAvg = 0;
+      for (byte x = 0; x < RATE_SIZE; x++)
+        beatAvg += rates[x];
+      beatAvg /= RATE_SIZE;
+    }
+  }
+
+  // Serial.print("IR=");
+  // Serial.print(irValue);
+  // Serial.print(", BPM=");
+  // Serial.print(beatsPerMinute);
+  // Serial.print(", Avg BPM=");
+  // Serial.print(beatAvg);
+
+  if (irValue < 50000)
+    Serial.print(" Is your finger on the sensor?");
+
+  Serial.println(beatAvg);
+}
